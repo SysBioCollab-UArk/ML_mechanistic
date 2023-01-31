2c69b5b14ef21362b0746a05104022156cd9c790
justin dykstra <jdykstra@uark.edu>
2023-01-31 05:48:31 -0600

    Area under curve & ic50 calculations & cell line selection

--- DataExtraction.py
+++ DataExtraction.py
@@ -1,16 +1,159 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import numpy as np
+import seaborn as sns
+
+#cell_line Key
+#0      A549       ACH-000681
+#1      AN3-CA     ACH-000940
+#2      CAPAN-2    ACH-000107
+#3      Daudi      ACH-000786
+#4      EFO-21     ACH-000308
+#5      HDLM-2     ACH-000267
+#6      HL-60      ACH-000002
+#7      HeLa       ACH-001086
+#8      K-562      ACH-000551
+#9      MCF7       ACH-000019
+#10     MOLT-4     ACH-001127
+#11     PC-3       ACH-000090
+#12     REH        ACH-000960
+#13     RPMI-8226  ACH-000817
+#14     RT4        ACH-000242
+#15     SiHa       ACH-000556
+#16     THP-1      ACH-000146
+#17     WM-115     ACH-000304
+
+# enter for x the cell_line number
+x=0
+
+
+
+name = pd.read_csv("data/doubling_times.csv")
+
+print(name.to_string())
+Name = name[['cell_line', 'depmap_id']]
+Name = Name.set_index("cell_line")
+Name = Name.drop_duplicates()
+print(Name)
+
+
+test = Name.to_dict("split")
+data = test
+new_dict = {}
+for i in range(len(data['index'])):
+    new_dict[data['index'][i]] = data['data'][i][0]
+
+print("new_dict",new_dict)
+
+
+#print(test)
+
+values_list = list(new_dict.values())
+keys_list = list(new_dict.keys())
+print(values_list)
+
 
 # making data frame from csv file
 data = pd.read_csv("data/sanger-viability.csv")
+data = data[['DRUG_NAME','DRUG_ID','COSMIC_ID','dose','viability','DATASET','ARXSPAN_ID']]
+
+
+filtered_df = data.loc[data['ARXSPAN_ID'].isin(values_list [x:x+1])]
+
+
+
 
 # retrieving rows by loc method
-data = data.set_index("ARXSPAN_ID")
-Cell = data.loc[["ACH-002148"]]
+#data = data.set_index("ARXSPAN_ID")
+
+
+Dname = "NAVITOCLAX"
+#Dname = "DOCETAXEL"
+
+
+
+Cell = filtered_df
+#Cell = data.loc[["ACH-002148"]]
 Cell = Cell.set_index("DRUG_NAME")
-Drug = Cell.loc[["DOCETAXEL"]]
+Drug = Cell.loc[[Dname]]
+Drug = Drug.set_index("DATASET")
+#Drug = Drug.loc[["GDSC1"]]
+Drug.sort_values(by='dose', ascending=True, inplace=True)
+
+
+
+
+
+
+
 
 # display
 print(data)
 print(Cell)
 print(Drug)
 print(Drug.to_string())
+
+
+# from data frame to numpy array
+viability = Drug['viability'].to_numpy()
+Conc = Drug['dose'].to_numpy()
+LogConc = np.log10(Conc)
+
+# viability plot
+plt.plot(LogConc, viability, "s", lw=2, label="viability")
+plt.legend(loc=0)
+
+# Curve fit viability
+
+def func(x, emax, ec, h):
+    eo = 1
+    return emax + (eo - emax) / (1 + (10 ** x / ec) ** h)
+
+
+
+popt, pcov = curve_fit(func, LogConc, viability, maxfev=500000)
+vEmax = popt[0]
+vEC = popt[1]
+vh = popt[2]
+vEo = 1
+vEi = (vEC ** vh) / (vEC ** vh + (10 ** LogConc) ** vh)
+plt.plot(LogConc, vEi, "-", lw=2)
+plt.xlabel(r'log$_{10}$ conc')
+plt.ylabel("relative effect")
+plt.title(Dname + ", " + keys_list[x])
+plt.tight_layout()
+
+
+# variables
+vEif = np.max(vEi)
+vEis = np.min(vEi)
+Lcf = np.max(LogConc)
+Lcs = np.min(LogConc)
+
+# area under curve
+dmin=10**Lcs
+dmax=10**Lcf
+
+vIC=vEC*(1/(1-2*vEis/vEif))**(1/vh)
+
+vAUC=(1/vh)*np.log10(abs((vEC**vh+dmin**vh)/dmin**vh*(dmax**vh/(vEC**vh+dmax**vh))))
+vAA=(1/vh)*np.log10(abs((vEC**vh+dmax**vh)/(vEC**vh+dmin**vh)))
+
+print("vArea under curve is ",vAUC)
+print("vActivity Area is ",vAA)
+print("vEC is ", vEC)
+print("vIC is ", vIC)
+
+
+
+# various plots
+
+#Drug.plot(x='dose', y='viability', kind='line')
+#plt.figure()
+Drug.plot.scatter(x='dose', y='viability')
+
+#Drug.groupby(['dose','DATASET'])['viability'].mean().plot(kind='bar')
+#plt.figure()
+#sns.heatmap(Drug.pivot_table(values='viability', index='DRUG_NAME', columns='dose'))
+plt.show()
--- Testing.py
+++ Testing.py
@@ -1,26 +1,42 @@
-import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
-sns.set_theme(style="whitegrid")
+import seaborn as sns
 
-# Initialize the matplotlib figure
-f, ax = plt.subplots(figsize=(6, 15))
+# Read in the "doubling_times.csv" file
+df = pd.read_csv("data/doubling_times.csv")
 
-# Load the example car crash dataset
-crashes = sns.load_dataset("car_crashes").sort_values("total", ascending=False)
-print(crashes)
-# Plot the total crashes
-sns.set_color_codes("pastel")
-sns.barplot(x="total", y="abbrev", data=crashes,
-            label="Total", color="b")
+# Filter the "cell_line" and "depmap_id" columns into a new dataframe
+cell_df = df[['cell_line', 'depmap_id']].drop_duplicates().set_index("cell_line")
 
-# Plot the crashes where alcohol was involved
-sns.set_color_codes("muted")
-sns.barplot(x="alcohol", y="abbrev", data=crashes,
-            label="Alcohol-involved", color="b")
+# Convert the dataframe into a dictionary with cell_line as key and depmap_id as value
+cell_dict = cell_df.to_dict("split")['data']
+cell_dict = {k: v[0] for k, v in cell_dict.items()}
 
-# Add a legend and informative axis label
-ax.legend(ncol=2, loc="lower right", frameon=True)
-ax.set(xlim=(0, 24), ylabel="",
-       xlabel="Automobile collisions per billion miles")
-sns.despine(left=True, bottom=True)
+# Read in the "sanger-viability.csv" file
+data = pd.read_csv("data/sanger-viability.csv")
+
+# Filter the columns to include "DRUG_NAME","dose","viability","ARXSPAN_ID"
+data = data[['DRUG_NAME','dose','viability','ARXSPAN_ID']]
+
+# Filter rows where ARXSPAN_ID is in the cell_dict
+data = data[data['ARXSPAN_ID'].isin(cell_dict.values())]
+
+# Set the index to "DRUG_NAME"
+data = data.set_index("DRUG_NAME")
+
+# Filter only rows with "DOCETAXEL"
+docetaxel_df = data.loc[["DOCETAXEL"]]
+
+# Plot line and scatter plots of the viability against dose
+docetaxel_df.plot(x='dose', y='viability', kind='line')
+docetaxel_df.plot.scatter(x='dose', y='viability')
+
+# Plot bar plot of mean viability grouped by DRUG_NAME
+docetaxel_df.groupby('DRUG_NAME')['viability'].mean().plot(kind='bar')
+
+# Plot heatmap of pivot table of viability indexed by DRUG_NAME and columns dose
+sns.heatmap(docetaxel_df.pivot_table(values='viability', index='DRUG_NAME', columns='dose'))
+
+# Show all plots
 plt.show()
+
